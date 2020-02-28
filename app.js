c650b3cf51a98b4d2bdb3cc14126e95deba28ca8
Yutaka Kobayashi <made.in.yutaka@gmail.com>
2020-02-28 11:55:32 +0900

    検証したいためコメントを追加

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 'use strict';
+// コメント追加
 // node.js で用意されている module を使用して実装する
 // fs: FileSystem の略で、ファイルを扱うことが可能
 const fs = require('fs')
@@ -33,10 +34,10 @@ rl.on('close', () => {
     value.change = value.popu15 / value.popu10
   }
   const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
-    return pair2[1].change - pair1[1].change
+    return pair1[1].change - pair2[1].change
   })
-  const rankingStrings = rankingArray.map(([key, value]) => {
-    return key + ': ' + value.popu10 + '=>' + value.popu15 + '変化率:' + value.change
+  const rankingStrings = rankingArray.map(([key, value], index) => {
+    return index+1  + '位 ' + key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change
   })
   console.log(rankingStrings)
 })
